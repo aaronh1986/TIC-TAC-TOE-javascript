e665a950f194d431d6f0cb6be1f192c696dc17ad
aaronh1986 <aaronharbourne@gmail.com>
2021-06-12 15:32:27 +0000

    31:31

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('#board');
+let circleTurn; //If true it's circles turn... if false etc etc
 
-cellElements.forEach(cell => {
+startGame()
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, {once: true}) //This means.. only ever fire this event listener once.
-})
+  })
+  setBoardHoverClass()
+}
 
 function handleClick(e) {
-    console.log('clicked');
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass)
+    // Check for win
+    //Check for Draw
+    // Switch turns
+    swapTurns();
+    setBoardHoverClass();
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
+    if(circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    }else {
+        board.classList.add(X_CLASS)
+    }
 }
